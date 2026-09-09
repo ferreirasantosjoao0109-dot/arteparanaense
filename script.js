db72e769995e75d33fe7de16683cbc64846ba690
ferreirasantosjoao0109-dot <ferreira.santos.joao0109@escola.pr.gov.br>
2026-09-09 17:29:16 -0300

    ATUALIZEI SCRIPT.S

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+conts botao = documento.querySelector(#botaoMensagemInterativa);
+conts mensagem = documento.querySelector(#mensagemInterativa);
+botao.addEventListener("click", mostrarMensagem);
+function mostrarMensagem(){
+    mensagem.textContent =
+    "VocÊ acabou de criar uma interação com JavaSCRIPT!"
+}
+    
