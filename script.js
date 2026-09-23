ef0de433b8c59b2990c382a1b54bbbaeb6576f18
ferreirasantosjoao0109-dot <ferreira.santos.joao0109@escola.pr.gov.br>
2026-09-23 17:24:12 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const botoesCurtir = document.querySelectorAll(".curtir");
 botoesCurtir.forEach(function(botaoCurtir){
-    let curtir = false;
-    botaoCurtir.addEventListener("cliclk", curtir);
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
     function curtir (){
         const contador = botaoCurtir.querySelector("span");
-        if(curtir === false){
+        if(curtiu === false){
             contador.textContent++;
-            curtir = true;}
+            curtiu = true;}
             else{
                 contador.textContent==;
-                curtir = false;
+                curtiu = false;
             }
  }
  
